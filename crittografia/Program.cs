cb11a77020f0d60e7fac45274eae3f2e0398a436
manciolove <114284283+manciolove@users.noreply.github.com>
2025-12-09 10:38:31 +0100

    trasposizione modifica

--- crittografia/Program.cs
+++ crittografia/Program.cs
@@ -1,4 +1,6 @@
-﻿namespace crittografia
+﻿using System.Runtime.CompilerServices;
+
+namespace crittografia
 {
     internal class Program
     {
@@ -8,14 +10,49 @@
            
             for (int i = 0; i < parola.Length; i++)
             {
-               int j = 0;
+               int j = 0; int indFin;
                 while (parola[i] != alfabeto[j])
                 {
                     j++;
                 }
-                parola[i] = alfabeto[j + k];
+                if (j + k > alfabeto.Length - 1)
+                {
+                     indFin= k - (alfabeto.Length - j);  
+                    parola[i] = alfabeto[indFin];
+                }
+                else
+                {
+                    parola[i] = alfabeto[j + k];
+                }
+               
             }
             return parola;
+        }
+
+        //funzione Trasposizione
+        static char[] Trasposizione(char[] parola, int k)
+        {
+
+            char[] vetTrasp = new char[parola.Length];
+            int cont = 0;
+
+            for (int i = 0; i < parola.Length; i++)
+            {
+
+                if (i + k > parola.Length - 1)
+                {
+
+                    cont = k - (parola.Length - i);
+                    vetTrasp[cont] = parola[i];
+                }
+                else
+                {
+
+                    vetTrasp[i + k] = parola[i];
+                }
+            }
+
+            return vetTrasp;
         }
         static void Main(string[] args)
         {
@@ -27,8 +64,20 @@
             Console.WriteLine("inserisci la tua parola: ");
             p = Console.ReadLine();
             char[] parolaArray = p.ToCharArray();
-            char[] parolaSostituita = parolaArray;
-            Console.WriteLine(Cesare(parolaSostituita, a));
+            char[] parolaSostituita = Cesare (parolaArray, a, k);
+            Console.WriteLine(parolaSostituita);
+
+            //trasposizione
+            char[] parolaTraslazione = Trasposizione(parolaSostituita, k);
+
+            Console.Write("Parola con trasposizione: ");
+
+            for (int i = 0; i < parolaTraslazione.Length; i++)
+            {
+                Console.Write(parolaTraslazione[i]);
+            }
+
+
 
 
 
